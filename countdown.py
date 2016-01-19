175a0df811bad6ac743038d7d1c5881e76a677e8
Arial7 <riesinger.pascal@gmail.com>
2016-01-19 13:02:58 +0100

    19.01.16

---
+++ countdown.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+i = 10
+
+while i > 0:
+    print(i, end=" ")
+    i -= 1
+
+print("START!!!")
--- randturtle4.py
+++ randturtle4.py
@@ -5,17 +5,29 @@ from random import *
 
 winkel = numinput(0, 10, 40)
 laenge = numinput(1, 3, 15)
-schritte = numinput(1, 5, 50)
+
+
+speed(9)
+
+setup(800, 800)
+
+start = position()
 
 def zufallsschritt(wink, laen):
     rt(randint(winkel * -1, winkel))
     fd(randint(1, laenge))
-
-def zufallsweg(schritt, wink, laen):
-    for i in range(0, int(schritt)):
+def zufallsweg(wink, laen):
+    while distance(start) < 350:
         zufallsschritt(wink, laen)
+    stamp()
 
-zufallsweg(schritte, winkel, laenge)
+tracer(False)
+
+for i in range(200):
+    zufallsweg(winkel, laenge)
+    home()
+
+tracer(True)
 
 while True:
     a = 0
